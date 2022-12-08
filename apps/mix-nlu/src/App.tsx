e426d84aa2c74fe996665ae0408321975ca9fcfd
Joshi <Pranav.Joshi@nuance.com>
2022-12-08 19:36:55 +0530

    init turbo monorepo

---
+++ apps/mix-nlu/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "@mix/core";
+
+function App() {
+  return (
+    <div>
+      This is MIX nlu from turbo monorepo, below button is from shared package mix/core. <br />
+     <Button>Hello</Button>
+    </div>
+  );
+}
+
+export default App;
